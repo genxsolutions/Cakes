dfe88ecfa09d33f84bd106fb75f176856ded3109
Zeeshan Muhammad <mr.zeeshan@hotmail.com>
2024-02-01 05:34:14 +0000

    added UI's

---
+++ app/src/main/java/com/genxsol/cakesapp/ui/viewmodels/CakesViewModel.kt
@@ -0,0 +1,85 @@
+package com.genxsol.cakesapp.ui.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.genxsol.cakesapp.common.NoInternetException
+import com.genxsol.cakesapp.common.dispatcher.DispatcherProvider
+import com.genxsol.cakesapp.common.logger.Logger
+import com.genxsol.cakesapp.common.networkhelper.NetworkHelper
+import com.genxsol.cakesapp.data.model.CakesItem
+import com.genxsol.cakesapp.data.repository.CakesRepository
+import com.genxsol.cakesapp.ui.base.UIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CakesViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val cakesRepository: CakesRepository,
+    private val pager: Pager<Int, CakesItem>,
+    private val logger: Logger,
+    private val dispatcherProvider: DispatcherProvider,
+    private val networkHelper: NetworkHelper,
+) : ViewModel() {
+
+    private val _cakesStateItem = MutableStateFlow<UIState<List<CakesItem>>>(UIState.Empty)
+    val cakesStateItem: StateFlow<UIState<List<CakesItem>>> = _cakesStateItem
+
+    private val _cakesStateItemPaging = MutableStateFlow<PagingData<CakesItem>>(PagingData.empty())
+    val cakesStateItemPaging: StateFlow<PagingData<CakesItem>> = _cakesStateItemPaging
+
+    init {
+        fetchCakes()
+    }
+
+    fun fetchCakes() {
+        fetchCakesData()
+    //TODO enable paging for large data set in lazy loading
+    //fetchCakesWithPaging()
+    }
+
+    private fun fetchCakesWithPaging() {
+        viewModelScope.launch {
+            pager.flow.cachedIn(viewModelScope)
+                .collect {
+                    _cakesStateItemPaging.value = it
+                }
+        }
+    }
+
+    private fun fetchCakesData() {
+        viewModelScope.launch {
+            if (!networkHelper.isNetworkConnected()) {
+                _cakesStateItem.emit(
+                    UIState.Failure(
+                        throwable = NoInternetException()
+                    )
+                )
+                return@launch
+            }
+            _cakesStateItem.emit(UIState.Loading)
+            cakesRepository.getCakes().mapFilterCollect()
+        }
+    }
+    private suspend fun Flow<List<CakesItem>>.mapFilterCollect() {
+            this.flowOn(dispatcherProvider.io)
+            .catch {
+                _cakesStateItem.emit(UIState.Failure(it))
+            }
+            .collect {
+                _cakesStateItem.emit(UIState.Success(it))
+                logger.d("CakesViewModel", "Success")
+            }
+    }
+
+}
---
+++ app/src/test/java/com/genxsol/cakesapp/ui/viewmodels/CakesViewModelTest.kt
@@ -0,0 +1,83 @@
+package com.genxsol.cakesapp.ui.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.paging.Pager
+import app.cash.turbine.test
+import com.genxsol.cakesapp.common.dispatcher.DispatcherProvider
+import com.genxsol.cakesapp.common.dispatcher.TestDispatcherProvider
+import com.genxsol.cakesapp.common.logger.Logger
+import com.genxsol.cakesapp.common.logger.TestLogger
+import com.genxsol.cakesapp.common.networkhelper.NetworkHelper
+import com.genxsol.cakesapp.common.networkhelper.TestNetworkHelper
+import com.genxsol.cakesapp.data.model.CakesItem
+import com.genxsol.cakesapp.data.repository.CakesRepository
+import com.genxsol.cakesapp.ui.base.UIState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(MockitoJUnitRunner::class)
+class CakesViewModelTest {
+
+    @Mock
+    private lateinit var cakesRepository: CakesRepository
+
+    @Mock
+    private lateinit var cakesPagingSource: Pager<Int, CakesItem>
+
+    private lateinit var logger: Logger
+    private lateinit var dispatcherProvider: DispatcherProvider
+    private lateinit var networkHelper: NetworkHelper
+
+    @Before
+    fun setUp() {
+        logger = TestLogger()
+        dispatcherProvider = TestDispatcherProvider()
+        networkHelper = TestNetworkHelper()
+        Dispatchers.setMain(dispatcherProvider.main)
+    }
+
+    @Test
+    fun fetchCakes_whenRepositoryResponseSuccess_shouldSetSuccessUiState() {
+        runTest {
+            doReturn(flowOf(emptyList<CakesItem>()))
+                .`when`(cakesRepository)
+                .getCakes()
+            val savedStateHandle = SavedStateHandle()
+
+            val viewModel = CakesViewModel(
+                savedStateHandle,
+                cakesRepository,
+                cakesPagingSource,
+                logger,
+                dispatcherProvider,
+                networkHelper
+            )
+            viewModel.cakesStateItem.test {
+                assertEquals(UIState.Success(emptyList<CakesItem>()), awaitItem())
+                cancelAndIgnoreRemainingEvents()
+            }
+            verify(cakesRepository, Mockito.times(1)).getCakes()
+        }
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+}
