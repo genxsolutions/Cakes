3864f1b417ef4adbb7af43304fa2304047bf6e1d
Zeeshan Muhammad <mr.zeeshan@hotmail.com>
2024-02-01 16:37:15 +0000

    updated popup dialog

--- app/src/main/java/com/genxsol/cakesapp/ui/screens/PopupScreen.kt
+++ app/src/main/java/com/genxsol/cakesapp/ui/screens/PopupScreen.kt
@@ -2,36 +2,32 @@ package com.genxsol.cakesapp.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShowPopup(navController: NavController, text: String) {
     AlertDialog(
         onDismissRequest = { navController.popBackStack() },
-        properties = DialogProperties(dismissOnClickOutside = true)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = text,
-                color = Color.White,
-                textAlign = TextAlign.Center
-            )
-        }
-    }
+        text = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = text,
+                    textAlign = TextAlign.Center
+                )
+            }
+        },
+        confirmButton = {},
+        dismissButton = {}
+    )
 }
