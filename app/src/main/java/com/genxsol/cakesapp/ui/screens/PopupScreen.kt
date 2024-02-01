7b28522d478a02cbf1f66cf53c0e53d00f4352bf
Zeeshan Muhammad <mr.zeeshan@hotmail.com>
2024-02-01 16:47:12 +0000

    added more screenshots

--- app/src/main/java/com/genxsol/cakesapp/ui/screens/PopupScreen.kt
+++ app/src/main/java/com/genxsol/cakesapp/ui/screens/PopupScreen.kt
@@ -3,12 +3,14 @@ package com.genxsol.cakesapp.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -23,6 +25,7 @@ fun ShowPopup(navController: NavController, text: String) {
             ) {
                 Text(
                     text = text,
+                    modifier = Modifier.padding(top= 15.dp),
                     textAlign = TextAlign.Center
                 )
             }
