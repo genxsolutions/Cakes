3a4d9c10a778260473b9e6e09b868c7ab3fc1509
Zeeshan Muhammad <mr.zeeshan@hotmail.com>
2024-02-01 05:35:28 +0000

    added pager

---
+++ app/src/main/java/com/genxsol/cakesapp/ui/paging/CakesPagingSource.kt
@@ -0,0 +1,41 @@
+package com.genxsol.cakesapp.ui.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.genxsol.cakesapp.common.dispatcher.DispatcherProvider
+import com.genxsol.cakesapp.data.model.CakesItem
+import com.genxsol.cakesapp.data.repository.CakesRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CakesPagingSource @Inject constructor(
+    private val cakesRepository: CakesRepository,
+    private val dispatcherProvider: DispatcherProvider
+) : PagingSource<Int, CakesItem>() {
+
+    override fun getRefreshKey(state: PagingState<Int, CakesItem>): Int? {
+        return state.anchorPosition
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, CakesItem> {
+        val page = params.key ?: 1
+        lateinit var loadResult: LoadResult<Int, CakesItem>
+
+        try {
+            val articles = withContext(dispatcherProvider.io) {
+                cakesRepository.getCakes(page).first()
+            }
+            loadResult = LoadResult.Page(
+                data = articles,
+                prevKey = if (page == 1) null else page.minus(1),
+                nextKey = if (articles.isEmpty()) null else page.plus(1)
+            )
+        } catch (e: Exception) {
+            loadResult = LoadResult.Error(e)
+        }
+        return loadResult
+    }
+}
---
+++ app/src/test/java/com/genxsol/cakesapp/ui/paging/CakesPagingSourceTest.kt
@@ -0,0 +1,57 @@
+package com.genxsol.cakesapp.ui.paging
+
+import androidx.paging.PagingSource
+import com.genxsol.cakesapp.common.dispatcher.DispatcherProvider
+import com.genxsol.cakesapp.common.dispatcher.TestDispatcherProvider
+import com.genxsol.cakesapp.common.networkhelper.NetworkHelper
+import com.genxsol.cakesapp.common.networkhelper.TestNetworkHelper
+import com.genxsol.cakesapp.data.model.CakesItem
+import com.genxsol.cakesapp.data.repository.CakesRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.doThrow
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class CakesPagingSourceTest {
+
+    @Mock
+    private lateinit var cakesRepository: CakesRepository
+
+    private lateinit var pagingSource: CakesPagingSource
+    private lateinit var dispatcherProvider: DispatcherProvider
+    private lateinit var networkHelper: NetworkHelper
+
+    @Before
+    fun setUp() {
+        dispatcherProvider = TestDispatcherProvider()
+        networkHelper = TestNetworkHelper()
+        pagingSource = CakesPagingSource(cakesRepository, dispatcherProvider)
+    }
+
+
+    @Test
+    fun load_whenResponseFailed_shouldGiveError() {
+        runTest {
+            // Given
+            val page = 1
+            val error = RuntimeException("Fake error")
+            doThrow(error)
+                .`when`(cakesRepository)
+                .getCakes(page)
+
+            // When
+            val result = pagingSource.load(PagingSource.LoadParams.Refresh(page, 1, false))
+
+            // Then
+            val expected = PagingSource.LoadResult.Error<Int, CakesItem>(error)
+            assertEquals(expected.toString(), result.toString())
+        }
+    }
+
+}
