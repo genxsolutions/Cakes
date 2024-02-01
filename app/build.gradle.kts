1f74c3ae6e08bec6325b1f5774cde88060d8f968
Zeeshan Muhammad <mr.zeeshan@hotmail.com>
2024-02-01 12:11:21 +0000

    implement buildSrc module for centeralised dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,66 +48,63 @@ android {
 }
 
 dependencies {
+    // modules
+    implementation(project(Modules.utilities))
 
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.fragment:fragment-ktx:1.6.0")
+    // AndroidX
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.appcompat)
+    implementation(Dependencies.material)
+    implementation(Dependencies.constraintLayout)
+    implementation(Dependencies.fragmentKtx)
 
-    //Architectural Components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    // Architectural Components
+    implementation(Dependencies.lifecycleViewModelKtx)
 
-    //Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    // Coroutines
+    implementation(Dependencies.coroutinesCore)
+    implementation(Dependencies.coroutinesAndroid)
 
-    //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.5.0")
+    // Retrofit
+    implementation(Dependencies.retrofit)
+    implementation(Dependencies.retrofitConverterGson)
+    implementation(Dependencies.okHttpLoggingInterceptor)
 
-    //Navigation Components
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    // Navigation Components
+    implementation(Dependencies.navigationFragmentKtx)
+    implementation(Dependencies.navigationUiKtx)
 
-    //Dagger
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-compiler:2.44")
-    implementation("androidx.hilt:hilt-common:1.1.0")
-    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    // Dagger Hilt
+    implementation(Dependencies.daggerHilt)
+    kapt(Dependencies.hiltCompiler)
+    implementation(Dependencies.hiltCommon)
+    kapt(Dependencies.hiltCompilerCommon)
 
-    //paging 3
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
-    implementation("androidx.paging:paging-compose:3.3.0-alpha02")
+    // Paging 3
+    implementation(Dependencies.paging)
+    implementation(Dependencies.pagingCompose)
 
+    // Compose
+    implementation(Dependencies.activityCompose)
+    implementation(Dependencies.composeBom)
+    implementation(Dependencies.composeUi)
+    implementation(Dependencies.composeMaterial)
+    implementation(Dependencies.composeUiGraphics)
+    implementation(Dependencies.composeUiToolingPreview)
+    implementation(Dependencies.material3)
+    implementation(Dependencies.lifecycleViewModelCompose)
+    implementation(Dependencies.hiltNavigationCompose)
+    implementation(Dependencies.navigationCompose)
+    implementation(Dependencies.lifecycleRuntimeCompose)
+    implementation(Dependencies.coilCompose)
 
-    //compose
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("androidx.compose.material:material:1.5.4")
-    implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    //Test
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    testImplementation("org.mockito:mockito-core:5.3.1")
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    testImplementation("app.cash.turbine:turbine:0.12.1")
+    // Test
+    testImplementation(Dependencies.junit)
+    androidTestImplementation(Dependencies.junitExt)
+    androidTestImplementation(Dependencies.espressoCore)
+    testImplementation(Dependencies.mockitoCore)
+    testImplementation(Dependencies.coreTesting)
+    testImplementation(Dependencies.coroutinesTest)
+    testImplementation(Dependencies.turbine)
 
 }
